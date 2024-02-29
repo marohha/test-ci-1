ab0c5fc2962599dff007e1fea284d651ffed2316
Marohha <marohha@yandex.ru>
2024-02-29 17:23:37 +0300

    fixed errors

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,11 +1,9 @@
 import characterCheck from '../index';
-
 test.each([
     ['Mar', 90, 'healthy'],
     ['Mar', 45, 'wounded'],
     ['Mar', 10, 'critical'],
-]) 
-('Character %s health %i check', (name, health, expected) => {
+])('Character %s health %i check', (name, health, expected) => {
     const result = characterCheck(name, health);
     expect(result).toBe(expected);
 });
